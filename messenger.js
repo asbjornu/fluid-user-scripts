dcaec49ab7d1b954e18680812e50d39b03d893cb
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-06-15 09:14:12 +0200

    Made the Messenger user script a bit less verbose

--- messenger.js
+++ messenger.js
@@ -6,17 +6,15 @@ var count;
 setInterval(updateDockBadge, 500);
 
 function updateDockBadge() {
-
     var els = document.getElementsByClassName('_1ht3');
 
     if (els.length > count) {
         window.console.log('notifying');
-        for(i = 0; i < els.length - count; i++) {
+        for (i = 0; i < els.length - count; i++) {
             var notifEl = els[i];
             var titleel = notifEl.getElementsByClassName('_1ht6')[0];
             var textel = notifEl.getElementsByClassName('_1htf')[0];
-            window.console.log(titleel.innerText);
-            window.console.log(textel.innerText);
+            window.console.log(titleel.innerText, textel.innerText);
             var title = titleel.innerText;
             var text = textel.innerText;
             window.webkitNotifications.createNotification('', title, text).show();
@@ -24,12 +22,5 @@ function updateDockBadge() {
     }
 
     count = els.length;
-
-    if(count > 0) {
-        window.fluid.dockBadge = count;
-    }
-    else {
-        window.fluid.dockBadge = '';
-    }
-    console.log('count ' + count);
+    window.fluid.dockBadge = count > 0 ? count : '';
 }
