cf13962538878da857d92c41d53cd4c4aec44868
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2016-06-21 14:24:27 +0200

    Added "Now Playing" notifications to the SoundCloud user script

--- soundcloud.js
+++ soundcloud.js
@@ -1,24 +1,83 @@
-function reset() {
+function resetDockBadge() {
     if (arguments.length > 0) {
         console.log(arguments);
     }
 
     window.fluid.dockBadge = '\u25fc';
-    setTimeout(updateDockBadge, 500);
-    return null;
+    return false;
 }
 
-function updateDockBadge() {
+function update(state) {
+    state.playing = updatePlayingStatus();
+    state.playingNow = state.playing
+                     ? notifyPlayingNow(state.playingNow)
+                     : state.playingNow;
+
+    setTimeout(update, 500, state);
+}
+
+function notify(title, text) {
+    if (!window.Notification) {
+        console.log('Notifications not supported.');
+        return;
+    }
+
+    if (Notification.permission === 'denied') {
+        console.log('Notifications denied.')
+    } else if (Notification.permission === 'default') {
+        Notification.requestPermission(function() {
+            notify(text);
+        });
+    } else if (Notification.permission === 'granted') {
+        var image = null;
+        var notification = window.webkitNotifications.createNotification(image, title, text);
+
+        /*notification.onclick = function() {
+            alert(1);
+        };*/
+
+        // console.log(notification);
+
+        try {
+            notification.show();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+function notifyPlayingNow(playingPrevious) {
+    var playbackSoundBadge = document.querySelector('.playbackSoundBadge span[aria-hidden="true"]');
+
+    if (!playbackSoundBadge) {
+        console.error('Found no playback sound badge.')
+        return null;
+    }
+
+    var playingNow = playbackSoundBadge.innerText;
+
+    if (playingNow !== playingPrevious) {
+        console.log('New song detected. Notifying!', playingPrevious, playingNow)
+        var dashIndex = playingNow.indexOf('-');
+        var artist = dashIndex > -1 ? playingNow.substring(0, dashIndex - 1) : 'SoundCloud Now Playing'
+        var song = dashIndex > -1 ? playingNow.substring(dashIndex + 1) : playingNow;
+        notify(artist, song);
+    }
+
+    return playingNow;
+}
+
+function updatePlayingStatus() {
     var playControlsContainer = document.querySelector('.playControls.m-visible');
 
     if (!playControlsContainer) {
-        return reset('Found no visible play controls');
+        return resetDockBadge('Found no visible play controls.');
     }
 
     var playControls = document.getElementsByClassName('playControl');
 
     if (!playControls || playControls.length == 0) {
-        return reset('Found no play control');
+        return resetDockBadge('Found no play control.');
     }
 
     var playControl = playControls[0];
@@ -27,13 +86,14 @@ function updateDockBadge() {
 
     if (playing) {
         window.fluid.dockBadge = '\u25b6';
+        return true;
     } else if (paused) {
         window.fluid.dockBadge = 'II';
     } else {
-        return reset('The play control had an unexpected title.', playControl);
+        return resetDockBadge('The play control had an unexpected title.', playControl);
     }
 
-    setTimeout(updateDockBadge, 500);
+    return false;
 }
 
-setTimeout(updateDockBadge, 1000);
+setTimeout(update, 1000, {});
